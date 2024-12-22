a86d4460e80e25465ae1831db679ef9e435e4f92
Margarida <maguicpereira@gmail.com>
2024-12-22 01:45:09 +0000

    Add frontend setup with Create React App

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Header = () => {
+  return <h1>Orçamento Simplificado com BudgetBuddy</h1>;
+};
+
+export default Header;
--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
+import path from 'path';
 import userRoutes from './routes/userRoutes.js';
 import transactionRoutes from './routes/transactionRoutes.js';
 
@@ -9,7 +10,17 @@ dotenv.config();
 const app = express();
 app.use(bodyParser.json());
 
+// API Routes
 app.use('/api/user', userRoutes);
 app.use('/api/transaction', transactionRoutes);
+
+// Frontend
+const __dirname = path.resolve();
+
+app.use(express.static(path.join(__dirname, 'frontend/build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'frontend/build/index.html'));
+});
 
 export default app;
